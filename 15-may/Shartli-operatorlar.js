c8f8914fa326f1b35151a0bb7f1d8ce932c63438
Azamat-Developer <aroziboyev1066@gmail.com>
2026-05-15 16:54:04 +0500

    js

---
+++ 15-may/Shartli-operatorlar.js
@@ -0,0 +1,18 @@
+  let yosh = 12
+  if (yosh > 17) {
+    console.log("Siz voyaga yetgansiz");
+    
+  } else {
+    console.log("Siz voyaga yetmagansiz");
+  }
+    
+    
+//   }=========
+let son = 2 
+if (son % 2 === 2) {
+    console.log("Siz juft son kiritinggiz");
+    
+} else {
+    console.log("Siz toq son kiritingiz");
+    
+}
