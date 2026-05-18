63f569414da87a897cf87ceea2c9140a3d80bcc8
Azamat-Developer <aroziboyev1066@gmail.com>
2026-05-18 17:21:29 +0500

    js

--- 15-may/Shartli-operatorlar2.js
+++ 15-may/Shartli-operatorlar2.js
@@ -1,9 +1,23 @@
-let logn = prompt("login kiriting")
+// let logn = prompt("login kiriting")
 
-if (logn === "admin") {
-    console.log("Xush kelibsiz admin");
+// if (logn === "admin") {
+//     console.log("Xush kelibsiz admin");
+    
+// } else {
+//     console.log("login xato");
+    
+// }
+// ========sonlar tengliki
+let son = prompt ("birinchi soni kiriting")
+let son1 = prompt ("ikkinchi soni kiriting")
+
+if (son > son1) {
+    console.log("birinchi son katta");
+    
+} else if (son < son1) {
+    console.log("ikkinchi son katta");
     
 } else {
-    console.log("login xato");
+    console.log("ikkalasi ham teng");
     
-}
+} 
