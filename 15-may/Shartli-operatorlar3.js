bd91c2cbf0ab2d248b97012b6fec8df7808cd724
Azamat-Developer <aroziboyev1066@gmail.com>
2026-05-20 17:02:57 +0500

    js

---
+++ 15-may/Shartli-operatorlar3.js
@@ -0,0 +1,20 @@
+//! let son = 3
+// if (son > 0) {
+//     console.log(son +1);
+    
+// } else {
+//     console.log(son);
+    
+// }
+// ^====2
+let son1 = -2
+if (son1 > 0) {
+    console.log("Bu son musbat", son1 +1);
+    
+} else if (son1 === 0) {
+    console.log("bu son noll");
+    
+} else {
+    console.log("Bu son manfiy", son1 -2);
+    
+}
