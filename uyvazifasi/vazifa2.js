6c4411d6bd2e6d4dc5fdd9a35a13d9839b7ab86c
Azamat-Developer <aroziboyev1066@gmail.com>
2026-05-21 22:02:33 +0500

    js

--- uyvazifasi/vazifa2.js
+++ uyvazifasi/vazifa2.js
@@ -1,8 +1,17 @@
-let son = 2
-if (son % 2 === 0) {
-    console.log("Siz juft son kiritingiz");
+// !let son = 2
+// if (son % 2 === 0) {
+//     console.log("Siz juft son kiritingiz");
     
-} else {
-    console.log("Siz toq son kiritinggiz");
+// } else {
+//     console.log("Siz toq son kiritinggiz");
     
-}
+// }
+// ^===========
+// let sinf = 12
+// if (sinf < 11) {
+//     console.log("siz maktabda oqiysz");
+    
+// } else {
+//     console.log("siz maktabni bitirgansiz");
+    
+// }
