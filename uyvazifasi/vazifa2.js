b21564f24d713b2623fe39fa2a74756bf02043d8
Azamat-Developer <aroziboyev1066@gmail.com>
2026-05-21 22:19:22 +0500

    js

--- uyvazifasi/vazifa2.js
+++ uyvazifasi/vazifa2.js
@@ -6,7 +6,7 @@
 //     console.log("Siz toq son kiritinggiz");
     
 // }
-// ^===========
+// ^===========2
 // let sinf = 4
 // if (sinf < 11) {
 //     console.log("siz maktabda oqiysz");
@@ -15,3 +15,19 @@
 //     console.log("siz maktabni bitirgansiz");
     
 // }
+// &&==========3
+let havo = 20
+if (havo >= 30) {
+    console.log("Bugun havo yaxshi");
+    
+} else if (havo <= 20) {
+    console.log("Bugun havo salqin");
+    
+} else if (havo >= -1) {
+    console.log("Bugun havo soviq");
+    
+}
+else {
+    console.log("Bugun havo issiq");
+    
+}
