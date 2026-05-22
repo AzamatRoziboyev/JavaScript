1ebb8e4ac147a0cee5712a6b3e22c0616e5fb115
Azamat-Developer <aroziboyev1066@gmail.com>
2026-05-22 17:13:29 +0500

    js

--- 15-may/Shartli-operatorlar3.js
+++ 15-may/Shartli-operatorlar3.js
@@ -1,53 +1,53 @@
 //! let son = 3
 // if (son > 0) {
 //     console.log(son +1);
-    
+
 // } else {
 //     console.log(son);
-    
+
 // }
 // ^====2
 // let son1 = 0
 // if (son1 > 0) {
 //     console.log("Bu son musbat", son1 +1);
-    
+
 // } else if (son1 === 0) {
 //     console.log("bu son noll");
-    
+
 // } else {
 //     console.log("Bu son manfiy", son1 -2);
-    
+
 // } 
 // &&=====3
 // let son2 = -3
 // if (son2 > 0) {
 //     console.log("bu son musbat", son2 +1);
-    
+
 // } else if (son2 === 0) {
 //         son2 = 10 
 //         console.log(son2);
-        
+
 
 // } else {
 //     console.log("bu son manfiy", son2 -2);
-    
+
 // }         
 // **===========4
 // let a = 11
 // if (a % 2 !== 0) {
 //     console.log("a soni toq son");
-    
+
 // } else {
 //     console.log("a soni toq son emas");
-    
+
 // }
 //^ ===========5
 let A = 3
 let B = 2
 if (A > 2 && B <= 3) {
     console.log(true);
-    
+
 } else {
     console.log(false);
-    
+
 }
