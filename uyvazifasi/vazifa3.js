adcf953486d2124f5e3986de815d90365be70a51
Azamat-Developer <aroziboyev1066@gmail.com>
2026-05-24 23:00:21 +0500

    js

--- uyvazifasi/vazifa3.js
+++ uyvazifasi/vazifa3.js
@@ -1,1 +1,21 @@
-
+// let son = 2 
+// if (son > 0) {
+//     console.log(son +1);
+    
+// } else {
+//     console.log(son);
+    
+// }
+// ========2
+let son1 = -4
+if (son1 > 0) {
+    console.log(son1 +1);
+    
+} else if (son1 < 0) {
+    console.log(son1 -2);
+    
+} else {
+    son1 = 10
+    console.log(son1);
+    
+}
