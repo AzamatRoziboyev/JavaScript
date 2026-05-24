fc9c534944998b1a5ea77a6a5355f5f0a051090f
Azamat-Developer <aroziboyev1066@gmail.com>
2026-05-24 23:06:12 +0500

    js

--- uyvazifasi/vazifa3.js
+++ uyvazifasi/vazifa3.js
@@ -6,16 +6,25 @@
 //     console.log(son);
     
 // }
-// ========2
-let son1 = -4
-if (son1 > 0) {
-    console.log(son1 +1);
+//! ========2
+// let son1 = -4
+// if (son1 > 0) {
+//     console.log(son1 +1);
     
-} else if (son1 < 0) {
-    console.log(son1 -2);
+// } else if (son1 < 0) {
+//     console.log(son1 -2);
     
-} else {
-    son1 = 10
-    console.log(son1);
+// } else {
+//     son1 = 10
+//     console.log(son1);
     
-}
+// }
+// ^============
+// let katta = prompt ("Soni kiriting")
+// if (katta > 100) {
+//     console.log("Bu katta son");
+    
+// } else {
+//     console.log("Bu kickiki son");
+    
+// }
