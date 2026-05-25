e01b2f164feff8f16351059fe8f3c55954fe1f2b
Azamat-Developer <aroziboyev1066@gmail.com>
2026-05-25 17:11:06 +0500

    js

--- 25-may/switch1.js
+++ 25-may/switch1.js
@@ -25,37 +25,41 @@
 //         console.log("Noto'g'ri kun");
 // }
 // !============2
-// let oy = 1
-// switch (oy) {
-//     case oy >= oy && oy <= 2 || oy <= 12:
-//         console.log("Qish");
-        
-// }
+let oy = 1
+switch (oy) {
+    case oy >= 1 || oy <= 2 || oy === 12:
+        console.log("Qish");
+       break 
+    case oy >= 3 || oy <=5:
+        console.log("Baxor");
+    
+}
+
 
 
 
 
 // ^===========3
-let til = eng
-switch (til) {
-    case eng:
-        console.log("Hello");
-        break
-    case uzb:
-        console.log("Salom");
-        break
-    case rus:
-        console.log("Prevet");
-        break
-    case hind:
-        console.log("Namaste");
-        break
-    case korean:
-        console.log("Annyonghaseyo");
-        break
-    default:
-        console.log("boshqa til");
+// let til = "rus"
+// switch (til) {
+//     case "eng":
+//         console.log("Hello");
+//         break
+//     case "uzb":
+//         console.log("Salom");
+//         break
+//     case "rus":
+//         console.log("Prevet");
+//         break
+//     case "hind":
+//         console.log("Namaste");
+//         break
+//     case "korean":
+//         console.log("Annyonghaseyo");
+//         break
+//     default:
+//         console.log("boshqa til");
         
         
         
-}
+// }
