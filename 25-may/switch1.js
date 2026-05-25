1d96b0d55ef3c847c45c093014dad5f0d51e7f57
Azamat-Developer <aroziboyev1066@gmail.com>
2026-05-25 16:59:50 +0500

    js

--- 25-may/switch1.js
+++ 25-may/switch1.js
@@ -1,27 +1,58 @@
-let day = 4;
+// let day = 4;
 
-switch (day) {
-    case 1:
-        console.log("Dushanba");
-    case 2:
-        console.log("Seshanba");
-        break;
-    case 3:
-        console.log("Chorshanba");
+// switch (day) {
+//     case 1:
+//         console.log("Dushanba");
+//     case 2:
+//         console.log("Seshanba");
+//         break;
+//     case 3:
+//         console.log("Chorshanba");
+//         break
+//     case 4:
+//         console.log("Payshanba");
+//         break
+//     case 5:
+//         console.log("Juma");
+//         break
+//     case 6:
+//         console.log("Shanba");
+//         break
+//     case 7:
+//         console.log("Yakshanba");
+//         break
+//     default:
+//         console.log("Noto'g'ri kun");
+// }
+// !============2
+// let oy = 1
+// switch (oy) {
+//     case oy >= oy && oy <= 2 || oy <= 12:
+//         console.log("Qish");
+        
+// }
+
+
+
+
+// ^===========3
+let til = rus
+switch (til) {
+    case eng:
+        console.log(Hello);
         break
-    case 4:
-        console.log("Payshanba");
+    case uzb:
+        console.log(Salom);
         break
-    case 5:
-        console.log("Juma");
+    case rus:
+        console.log(Prevet);
         break
-    case 6:
-        console.log("Shanba");
+    case hind:
+        console.log(Namaste);
         break
-    case 7:
-        console.log("Yakshanba");
-        break
-    default:
-        console.log("Noto'g'ri kun");
+    case korean:
+        console.log(Annyonghaseyo);
+        
+        
+        
 }
-
