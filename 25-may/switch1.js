a858896c8f1c83256efde93506ab3f4cfbc1b280
Azamat-Developer <aroziboyev1066@gmail.com>
2026-05-25 16:40:02 +0500

    js

--- 25-may/switch1.js
+++ 25-may/switch1.js
@@ -1,1 +1,27 @@
-let son = 1
+let day = 4;
+
+switch (day) {
+    case 1:
+        console.log("Dushanba");
+    case 2:
+        console.log("Seshanba");
+        break;
+    case 3:
+        console.log("Chorshanba");
+        break
+    case 4:
+        console.log("Payshanba");
+        break
+    case 5:
+        console.log("Juma");
+        break
+    case 6:
+        console.log("Shanba");
+        break
+    case 7:
+        console.log("Yakshanba");
+        break
+    default:
+        console.log("Noto'g'ri kun");
+}
+
