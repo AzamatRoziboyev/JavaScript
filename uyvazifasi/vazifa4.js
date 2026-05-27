c71862900d5c88724bc6a2ca8037b0fa61d06125
Azamat-Developer <aroziboyev1066@gmail.com>
2026-05-27 23:41:45 +0500

    js

---
+++ uyvazifasi/vazifa4.js
@@ -0,0 +1,22 @@
+let til = "uz"
+switch (til) {
+    case "eng":
+        console.log("Hello");
+        break
+    case "rus":
+        console.log("Privet");
+        break
+    case "uz":
+        console.log("Salom");
+        break
+    case "hind":
+        console.log("Namaste");
+        break
+    case "korea":
+        console.log("Anixasiyo");
+        break
+    default:
+        console.log("Siz boshqa til kiriritingiz");
+        
+
+}
