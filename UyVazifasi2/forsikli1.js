2a41f7d64fe5b119ef2ec65dc951f0732fd563b3
Azamat-Developer <aroziboyev1066@gmail.com>
2026-06-03 22:45:05 +0500

    add

--- UyVazifasi2/forsikli1.js
+++ UyVazifasi2/forsikli1.js
@@ -10,9 +10,13 @@
 //    }
 // }
 // ^========5 ga 2 qoldiqsiz bolinadiganlar
-for (let b = 0; b <= 1000; b++) {
-    if (b % 5 !== 0 && b % 2 !== 0) {
-        console.log(b);
+// for (let b = 0; b <= 1000; b++) {
+//     if (b % 5 !== 0 && b % 2 !== 0) {
+//         console.log(b);
         
-    }
+//     }
+// }
+for (let i = 10; i >= 1; i--) {
+    console.log(i);
+    
 }
