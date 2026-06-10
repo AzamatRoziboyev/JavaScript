32f32a62a6fddb3bb46c8168353c4137ea6ce912
Azamat-Developer <aroziboyev1066@gmail.com>
2026-06-10 17:27:03 +0500

    add

--- 10-iyun/while1.js
+++ 10-iyun/while1.js
@@ -5,4 +5,13 @@
     
 // }
 //! ========1 dan 20 gacha sonlarni yigindisini topish
+let son1 = 1
+let natija = 0
+while (son1 <= 20) {
+    console.log(son1);
+    natija = natija + son1
+    son1 = son1 + 1
 
+    
+}
+console.log(natija)
