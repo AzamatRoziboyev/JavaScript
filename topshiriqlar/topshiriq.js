b77d5bad79b0c013a408f778e2faad5abd25a8be
Azamat-Developer <aroziboyev1066@gmail.com>
2026-06-12 16:35:21 +0500

    add

--- topshiriqlar/topshiriq.js
+++ topshiriqlar/topshiriq.js
@@ -1,6 +1,15 @@
-let son = 5 
-son1 = 7 
-{
-    console.log(son + son1);
+// let son = 5 
+// son1 = 7 
+// {
+//     console.log(son + son1);
     
-}
+// }
+// *=======2
+// let a = 12
+// b = 4
+// {
+//     console.log(a - b);
+    
+// }
+// *=======3
+
