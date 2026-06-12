fc918583a00d90b766144a2762bada0a31c4c1fd
Azamat-Developer <aroziboyev1066@gmail.com>
2026-06-12 18:04:36 +0500

    add

--- topshiriqlar/topshiriq.js
+++ topshiriqlar/topshiriq.js
@@ -2,36 +2,107 @@
 // son1 = 7 
 // {
 //     console.log(son + son1);
-    
+
 // }
 // *=======2
 // let a = 12
 // b = 4
 // {
 //     console.log(a - b);
-    
+
 // }
 // *=======3
 // let son = 8 
 // son1 = 3
 // {
 //     console.log(son * son1);
-    
+
 // }
 // *=========4
 // let son = 20
 // son1 = 4
 // {
 //     console.log(son / son1);
-    
+
 // }
 // *=======5 
 // for (let i = 0; i < 3; i++) {
 //     console.log("salom");
-    
+
 // }
 // *======6 
-for (let son = 1; son <= 5; son++) {
-    console.log(son);
+// for (let son = 1; son <= 5; son++) {
+//     console.log(son);
+
+// }
+// *======7 while
+// let son = 10
+// while (son >= 6) {
+//     console.log(son);
+//     son--
+
+// }
+// *======8 if else
+// let son = 14
+// if (son >= 10) {
+//     console.log("katta");
     
-}
+// } else {
+//     console.log("kichik");
+    
+// }
+// *=======9 
+// let a = 5
+// if (a === 5) {
+//     console.log("teng");
+    
+// } else {
+//     console.log("teng emas");
+    
+// }
+// *======10 (for) yegindini toping
+// let yigindi = 0
+// for (let son = 1; son <= 4; son++)  {
+//     yigindi = yigindi + son 
+// }
+// console.log(yigindi);
+// *=======11 (if)
+// let i = 7
+// if (i >= 0) {
+//     console.log("musbat");
+    
+// } else {
+//     console.log("manfiy");
+    
+// }
+// *=======12 
+// for (let i = 2; i <= 8; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i);
+        
+//     }
+// }
+// *======13 while da kopaytma chiqarish
+// let son = 1, kop = 1
+// while (son <= 10) {
+//     kop = kop * son
+//     son = son + 1
+// }
+// console.log(kop);
+// *=========14
+// let son = 12
+// son1 = 8 
+// if (son > son1 ) {
+//     console.log("son katta");
+    
+// } else {
+//     console.log("kichik");
+    
+// }
+// *========15 sozlarni nechta harf borligni aniqlash
+// let uzunligi = "JavaScript" 
+// console.log(uzunligi. length);
+
+
+
+
