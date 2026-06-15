0f40add780d69c2fbdb6490e208e18134534093c
Azamat-Developer <aroziboyev1066@gmail.com>
2026-06-15 16:58:42 +0500

    add

--- 10-iyun/DoWhile.js
+++ 10-iyun/DoWhile.js
@@ -1,8 +1,17 @@
 // *juft sonlarni kivadiratini chiqarish
+// let i = 1
+// do {
+//     if (i % 2 == 0) {
+//         console.log(i ** 2);
+        
+//     }
+//     i++
+// } while (i < 100)
+// *toq sonlarni qubni chiqarish
 let i = 1
 do {
-    if (i % 2 == 0) {
-        console.log(i ** 2);
+    if (i % 2 !== 0) {
+        console.log(i ** 3);
         
     }
     i++
