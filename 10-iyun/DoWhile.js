69be467fe0f940d2575a0fb8544892687fae2a3a
Azamat-Developer <aroziboyev1066@gmail.com>
2026-06-15 17:32:57 +0500

    add

--- 10-iyun/DoWhile.js
+++ 10-iyun/DoWhile.js
@@ -18,9 +18,37 @@
 // } while (i < 100)
 // 
 // *5 dan 1 gacha sonlarni chiqarish
-let i = 5 
-do {
-    console.log(i);
-    i--
+// let i = 5 
+// do {
+//     console.log(i);
+//     i--
     
-} while (i >= 5)
+// } while (i >= 1)
+// *hello sozini 3 marta chiqarish
+// let i = 1 
+// do {
+//     console.log("hello");
+//     i++
+    
+// } while (i <= 3)
+// *7 soni kiritguncha kirmasin 
+// let i = 7
+// do {
+//     if (i === 7) {
+//         console.log("toxtang");
+        
+//     }
+//     i++
+    
+// } while (i === 7) 
+//     console.log("xato");
+// *1 dan 10 gacha faqat juft sonlarni chiqarish
+let i = 1
+do {
+    if (i % 2 == 0) {
+        console.log(i);
+        
+    } 
+    i++
+} while (i <= 10)
+    
