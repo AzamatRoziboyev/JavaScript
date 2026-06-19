cbc214fafc260130446d3676519f1a0825a5803b
Azamat-Developer <aroziboyev1066@gmail.com>
2026-06-19 16:41:46 +0500

    add

---
+++ 19-iyun/array.js
@@ -0,0 +1,9 @@
+let names = ["Saidjon", "Shohjahon", "Azamat", "suxrob", "Jahongir"]
+// console.log(names);
+console.log(names[0])
+console.log(names[1])
+console.log(names[2])
+console.log(names[3])
+console.log(names[4])
+
+
