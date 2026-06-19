4a358d75e1fc6e519ef6507a061ec44c3cde41b9
Azamat-Developer <aroziboyev1066@gmail.com>
2026-06-19 16:46:48 +0500

    add

--- 19-iyun/array.js
+++ 19-iyun/array.js
@@ -5,5 +5,15 @@ console.log(names[1])
 console.log(names[2])
 console.log(names[3])
 console.log(names[4])
+// *==============uzunligi 4ga teng bo'sh array
+let massiv = new Array(4)
+let massiv0 = new Array(4,1,2,3,5)
+let massiv1 =
+console.log(massiv);
+console.log(massiv0);
+console.log(massiv1);
+console.log(names.length);
+
+
 
 
