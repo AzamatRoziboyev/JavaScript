55eaec05724c4020a0a8456ff008bcb02c371764
Azamat-Developer <aroziboyev1066@gmail.com>
2026-06-21 16:39:46 +0500

    add

--- topshiriqlar/topshiriqlar2.js
+++ topshiriqlar/topshiriqlar2.js
@@ -3,3 +3,17 @@
 // for (let i of sonlar) {
 //     console.log(i)
 // }
+
+// *Oxiriga element qo'shish
+// let arr = [10, 20, 30]
+// let add = arr.push(40)
+// console.log(arr);
+
+// *Oxirgi elementni o'chirish
+// let arr = [5, 10, 15, 20]
+// let a = arr.pop(20)
+// console.log(arr);
+
+
+
+
