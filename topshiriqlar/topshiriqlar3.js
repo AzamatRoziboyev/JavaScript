9cbb2f17bf21ed4ca3e736d3c71dbde62bb28fb9
Azamat-Developer <aroziboyev1066@gmail.com>
2026-06-22 16:53:35 +0500

    add

--- topshiriqlar/topshiriqlar3.js
+++ topshiriqlar/topshiriqlar3.js
@@ -2,7 +2,7 @@
 //     console.log(i);
     
 // }
-// *whileda faqat juft sonlarni chiqarish
+// *whileda faqat juft sonlarni chiqarish {2}
 // let i = 1
 // while (i <= 20) {
 //     if (i % 2 === 0) {
@@ -11,12 +11,17 @@
 //     }
 //     i++
 // }
-// **foydalanuvchi 0 yozmaguncha kiritilmasin 
+// **foydalanuvchi 0 yozmaguncha kiritilmasin {3}
 // let son = 0
 // do {
 //     son = parseInt(prompt("Soni kiriting"))
 // } while (son !== 0)
 //     console.log(0);
-
+//* Berilgan massivning barcha elementlarini for yordamida chiqaring {4}
+// let arr = ["Shoxruz", 12, "46", false, true, undefined, Boolean, Number, String]    
+// for (let i of arr) {
+//     console.log(i);
     
+// }
+
 
