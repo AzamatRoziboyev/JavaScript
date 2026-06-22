b424cfa0f7efd7edc067131e636ca563c2f285d8
Azamat-Developer <aroziboyev1066@gmail.com>
2026-06-22 16:34:47 +0500

    add

--- topshiriqlar/topshiriqlar3.js
+++ topshiriqlar/topshiriqlar3.js
@@ -1,2 +1,14 @@
-let i = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);
+    
+// }
+// *whileda faqat juft sonlarni chiqarish
+let i = 1
+while (i <= 20) {
+    if (i % 2 === 0) {
+        console.log(i);
+        
+    }
+    i++
+}
 
