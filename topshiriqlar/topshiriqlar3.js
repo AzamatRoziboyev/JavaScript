704c385fb0cb64c27b510f1b5ebf556e942d0e36
Azamat-Developer <aroziboyev1066@gmail.com>
2026-06-25 16:52:27 +0500

    add

--- topshiriqlar/topshiriqlar3.js
+++ topshiriqlar/topshiriqlar3.js
@@ -47,13 +47,13 @@
 // }
 // console.log(k);
 // * 1 dan 50 gacha bo'lgan toq sonlarning yig'indisini for yordamida toping {11}
-// let sum = 0
-// for (let i = 1; i < 50; i++) {
-//     if (i % 2 !== 0)
-//         console.log(i)
-//     i++
-//     sum += i        
-// }
+let sum = 0
+for (let i = 1; i < 50; i++) {
+    if (i % 2 !== 0)
+        console.log(i)
+    i++
+    sum += i        
+}
 
 
 
