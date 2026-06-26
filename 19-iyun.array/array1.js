6b9fa1317fd1792ae33045ba55806738cdc24667
Azamat-Developer <aroziboyev1066@gmail.com>
2026-06-26 17:40:23 +0500

    add

--- 19-iyun.array/array1.js
+++ 19-iyun.array/array1.js
@@ -19,3 +19,7 @@
 // arr.shift()
 // console.log(arr);
 
+//* oratadaki elementni olib orniga yangi element qoyish
+let arr = [23, 54, 76, 54, 346, 234, 213, 12, 31, 3, 4, 5, 345, 3, 4, 53, 54]
+arr.splice(1, 15, 7)
+console.log(arr);
