1a661d23af6e29d9400da19ad5a307b7bd08f203
Azamat-Developer <aroziboyev1066@gmail.com>
2026-06-26 17:32:27 +0500

    add

--- 19-iyun.array/array1.js
+++ 19-iyun.array/array1.js
@@ -15,4 +15,7 @@
 // console.log(arr);
 
 //* boshidan elementni 1 tasni olib tashlash
+// let arr = [23, 54, 76, 54, 346, 234, 213, 12, 31, 3, 4, 5, 345, 3, 4, 53, 54]
+// arr.shift()
+// console.log(arr);
 
