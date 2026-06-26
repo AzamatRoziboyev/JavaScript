e268a2a3a563c7210e2fa9a1e42fbd2afb1075cb
Azamat-Developer <aroziboyev1066@gmail.com>
2026-06-26 17:19:15 +0500

    add

--- 19-iyun.array/array1.js
+++ 19-iyun.array/array1.js
@@ -1,4 +1,18 @@
 //* oxiriga element qoshish
-let arr = [23, 54, 76, 54, 346, 234, 213, 12, 31, 3, 4, 5, 345, 3, 4, 53, 54]
-arr.push(1, 2, 3, 4, 5)
-console.log(arr);
+// let arr = [23, 54, 76, 54, 346, 234, 213, 12, 31, 3, 4, 5, 345, 3, 4, 53, 54]
+// arr.push(1, 2, 3, 4, 5)
+// console.log(arr);
+
+//* oxiriga element olib tashlash
+
+// let arr = [23, 54, 76, 54, 346, 234, 213, 12, 31, 3, 4, 5, 345, 3, 4, 53, 54]
+// arr.pop()
+// console.log(arr);
+
+//* boshga element qoshish
+// let arr = [23, 54, 76, 54, 346, 234, 213, 12, 31, 3, 4, 5, 345, 3, 4, 53, 54]
+// arr.unshift(8, 2, )
+// console.log(arr);
+
+//* boshidan elementni 1 tasni olib tashlash
+
