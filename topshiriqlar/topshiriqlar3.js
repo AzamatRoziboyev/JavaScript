0deb437dfd24cce381dfb0c062d83d4844490f30
Azamat-Developer <aroziboyev1066@gmail.com>
2026-06-30 22:38:48 +0500

    add

--- topshiriqlar/topshiriqlar3.js
+++ topshiriqlar/topshiriqlar3.js
@@ -101,6 +101,28 @@
 //  }
 //  let average = sum / arr.length
 //  console.log("ortacha qiymat", average);
+//* Berilgan massivdagi barcha musbat sonlarning yig'indisini for yordamida toping {23}
+//& ozimki
+// let arr = [1,2,3,4,5,6,7,]
+// let sum = 0;
+// for (let i of arr) {
+//     if (i > 0) {
+//              console.log(i)
+//               sum +=  i
+//     }
+// }
+// console.log(sum);
+//& chatgpt niki
+// let arr = [1,2,3,4,5,6,7];
+// let sum = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > 0) {
+//         sum += arr[i];
+//     }
+// }
+
+// console.log("Musbat sonlar yig'indisi:", sum);
  
 
 
