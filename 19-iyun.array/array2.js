7ade12e09ce2bd2e08cc095b1136689436f00675
Azamat-Developer <aroziboyev1066@gmail.com>
2026-07-01 17:19:58 +0500

    add

--- 19-iyun.array/array2.js
+++ 19-iyun.array/array2.js
@@ -1,11 +1,11 @@
 let arr = [1, 2, [3, 4, 5, [6, 7], [8, 9]], 10, 11, [12, [13, [14, [15, 16]], 17, [18, 19], 20], 21], 23];
-// console.log(arr [6]); {23}
-// console.log(arr[5][2]); {21}
-// console.log(arr[4]); {11}
-// console.log(arr[5][1][3][1]); {19}
-// console.log(arr[5][1][2]); {17}
-// console.log(arr[5][1][4]); {20}
-console.log(arr[]);
+console.log(arr[5][1][1][1][0]); {15}
+console.log(arr[5][1][2]); {17}
+console.log(arr[4]); {11}
+console.log(arr[5][1][3][1]); {19}
+console.log(arr[5][1][4]); {20}
+console.log(arr[5][2]); {21}
+console.log(arr [6]); {23}
 
 
 
