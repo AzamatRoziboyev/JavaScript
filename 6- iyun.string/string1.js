e97f5747c748d61a7a50a867d97523a584bbf7c5
Azamat-Developer <aroziboyev1066@gmail.com>
2026-07-06 17:06:08 +0500

    add

--- 6- iyun.string/string1.js
+++ 6- iyun.string/string1.js
@@ -12,7 +12,7 @@ let matn = "Kecha juda kuchli yomg'ir shamol bo'ldi va tinmay 2soat yomg'ir yog'
 //*includes/startsWith/endsWith elemintni bor yoki yoqlikini aniklardi
 console.log(matn.includes("yomg'irlar")); //false
 console.log(matn.startsWith("Kecha"));//boshidan aniqlaydi true
-console.log(matn.endsWith("!")); //oxiridan 
+console.log(matn.endsWith("!")); //oxiridan aniqlaydi true
 
 
 
