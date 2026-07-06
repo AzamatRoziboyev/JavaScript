239fe0b2b13b08a037a13faff4ec02d320492576
Azamat-Developer <aroziboyev1066@gmail.com>
2026-07-06 17:05:44 +0500

    add

--- 6- iyun.string/string1.js
+++ 6- iyun.string/string1.js
@@ -9,8 +9,12 @@ let matn = "Kecha juda kuchli yomg'ir shamol bo'ldi va tinmay 2soat yomg'ir yog'
 //  console.log(matn.indexOf("yomg'ir"));
 //  console.log(matn.lastIndexOf("yomg'ir"));
 
-//*includes
+//*includes/startsWith/endsWith elemintni bor yoki yoqlikini aniklardi
 console.log(matn.includes("yomg'irlar")); //false
+console.log(matn.startsWith("Kecha"));//boshidan aniqlaydi true
+console.log(matn.endsWith("!")); //oxiridan 
+
+
 
  
 
