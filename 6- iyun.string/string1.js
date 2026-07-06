425992ac635cc541b5c83509be75e6c4a31f35c1
Azamat-Developer <aroziboyev1066@gmail.com>
2026-07-06 16:48:33 +0500

    addd

--- 6- iyun.string/string1.js
+++ 6- iyun.string/string1.js
@@ -1,5 +1,5 @@
 let matn = "Kecha juda kuchli shamol bo'ldi va tinmay 2soat yomg'ir yog'di!"
-let kattaharf = matn.toUpperCase()
-console.log(kattaharf);
+let kattaHarf = matn.toUpperCase()
+console.log(kattaHarf);
 console.log(matn);
 
