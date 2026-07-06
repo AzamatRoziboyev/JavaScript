67af78a3c4533576c86a8ea6a6aa38a6ba55d29a
Azamat-Developer <aroziboyev1066@gmail.com>
2026-07-06 17:00:02 +0500

    add

--- 6- iyun.string/string1.js
+++ 6- iyun.string/string1.js
@@ -6,9 +6,12 @@ let matn = "Kecha juda kuchli yomg'ir shamol bo'ldi va tinmay 2soat yomg'ir yog'
 // console.log(matn);
 
 //*indexof - lastIndexof
- console.log(matn.indexOf("yomg'ir"));
- console.log(matn.lastIndexOf("yomg'ir"));
- 
+//  console.log(matn.indexOf("yomg'ir"));
+//  console.log(matn.lastIndexOf("yomg'ir"));
+
+//*includes
+console.log(matn.includes("yomg'irlar"));
+
  
 
 
