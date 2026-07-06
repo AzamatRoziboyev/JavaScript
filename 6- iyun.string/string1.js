8627bf6449f09c784191aa484e9abaeaa4676bc8
Azamat-Developer <aroziboyev1066@gmail.com>
2026-07-06 16:56:50 +0500

    add

--- 6- iyun.string/string1.js
+++ 6- iyun.string/string1.js
@@ -1,7 +1,15 @@
-// let matn = "Kecha juda kuchli shamol bo'ldi va tinmay 2soat yomg'ir yog'di!"
+let matn = "Kecha juda kuchli yomg'ir shamol bo'ldi va tinmay 2soat yomg'ir yog'di!"
 // let kattaHarf = matn.toUpperCase()
 // let kichikHarf = matn.toLowerCase()
 // console.log(kichikHarf);
 // console.log(kattaHarf);
 // console.log(matn);
+
+//*indexof - lastIndexof
+ console.log(matn.indexOf("yomg'ir"));
+ console.log(matn.lastIndexOf("yomg'ir"));
+ 
+ 
+
+
 
