f23585ccefd89e8d7681f11e1b46b0cd3879bc68
Azamat-Developer <aroziboyev1066@gmail.com>
2026-07-06 17:28:42 +0500

    as

--- 6- iyun.string/string1.js
+++ 6- iyun.string/string1.js
@@ -13,4 +13,3 @@ let matn = "Kecha juda kuchli yomg'ir shamol bo'ldi va tinmay 2soat yomg'ir yog'
 // console.log(matn.includes("yomg'irlar")); //false
 // console.log(matn.startsWith("Kecha"));//boshidan aniqlaydi true
 // console.log(matn.endsWith("!")); //oxiridan aniqlaydi true
-
