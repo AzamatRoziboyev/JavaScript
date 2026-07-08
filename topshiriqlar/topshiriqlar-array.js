e62159bb9120759a345b33a0d13c4f907f695d32
Azamat-Developer <aroziboyev1066@gmail.com>
2026-07-08 16:45:41 +0500

    add

--- topshiriqlar/topshiriqlar-array.js
+++ topshiriqlar/topshiriqlar-array.js
@@ -365,4 +365,4 @@
 //*57-topshiriq (filter)
 let arr = [{ism:"Ali", yosh:17}, {ism:"Vali", yosh:22}, {ism:"Guli", yosh:15}];
 let yosh = arr.filter(n => n.yosh > 18) 
-console.log(yosh);
+console.log(yosh);  
