19594e6c6ed18ba898dc1dcbd0effcb3d6575eb6
Azamat-Developer <aroziboyev1066@gmail.com>
2026-07-09 22:46:29 +0500

    add

--- 6- iyun.string/string2.js
+++ 6- iyun.string/string2.js
@@ -1,6 +1,64 @@
 //* 1 topshiriq
-let arr = "salom dunyo"
-let kattaHarf = arr.toUpperCase("salom dunyo")
-console.log(kattaHarf);
+// let arr = "salom dunyo"
+// let kattaHarf = arr.toUpperCase("salom dunyo")
+// console.log(kattaHarf);
 
 //* 2 topshiriq
+// let arr = "JavaScript"
+// console.log(arr.length);
+
+//* 3 topshiriq
+// let arr =  "Dasturlash"
+// console.log(arr.indexOf("r"));
+
+//* 4 topshiriq
+// let arr = "Hello world"
+// console.log(arr.startsWith("Hello"));
+
+//* 5 topshiriq
+// let arr = "WEB DASTURLASH"
+// let kichikHarf = arr.toLowerCase("WEB DASTURLASH")
+// console.log(kichikHarf);
+
+//* 6 topshiriq
+// let arr = "Kompyuter"
+// console.log(arr.charAt(3));
+
+//* 7 topshiriq
+// let arr = "   Salom   "
+// console.log(arr.trim());
+ 
+//* 8 topshiriq
+// let arr = "Assalomu alaykum!"
+// console.log(arr.endsWith("!"));
+
+//* 9 topshiriq
+// let arr = "Men js o'rganyapman"
+// console.log(arr.includes("js"));
+
+//* 10 topshiriq
+// let arr =  "ha"
+// console.log("ha".repeat(3));
+
+//*11 topshiriq
+// let ism = "Ali";
+// let familiya = "Karimov";
+// let toliqIsm = ism.concat(" ", familiya);
+// console.log(toliqIsm);
+
+//* 12 topshiriq
+// let arr = "javascript"
+// let son = arr[0].toUpperCase() + arr.slice(1)
+// console.log(son);
+
+//* 13 topshiriq
+// let arr = "Men dastur yozaman"
+// console.log(arr.split(" "));
+
+//* 14 topshiriq
+// let arr = "salom"
+// console.log(arr.replaceAll("a", "o"));
+
+//* 15 topshiriq
+let arr = "Dasturlash"
+
