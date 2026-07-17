a01f5d4116ca85e3fbe1d8697d899de69396c984
Azamat-Developer <aroziboyev1066@gmail.com>
2026-07-17 17:32:13 +0500

    add

--- 17-iyul.Functinon/functions.js
+++ 17-iyul.Functinon/functions.js
@@ -1,17 +1,17 @@
 //*Function Decloration
-function bir(a, b,) {
-    console.log(a*b);
-    console.log(a / b);
+// function bir(a, b,) {
+//     console.log(a*b);
+//     console.log(a / b);
     
-}
-bir(5, 10)
+// }
+// bir(5, 10)
 
 //* Function Expression
-const kopaytma = function (a, b) {
-    console.log(a * b);
-    console.log(a / b);
+// const kopaytma = function (a, b) {
+//     console.log(a * b);
+//     console.log(a / b);
     
-}
+// }
 kopaytma(5, 10)
 
 //* Arrow  function
