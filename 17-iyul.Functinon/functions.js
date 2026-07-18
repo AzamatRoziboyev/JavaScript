1b35d84cd32a80fa11464d3d7e4b784630c675e0
Azamat-Developer <aroziboyev1066@gmail.com>
2026-07-18 23:14:24 +0500

    add

--- 17-iyul.Functinon/functions.js
+++ 17-iyul.Functinon/functions.js
@@ -15,7 +15,7 @@
 // kopaytma(5, 10)
 
 //* Arrow  function
-const bolinma = (a, b,) => a * b
-const bo = (a, b) => a / b
-console.log(bolinma(5, 10), bo(5, 10));
+// const bolinma = (a, b,) => a * b
+// const bo = (a, b) => a / b
+// console.log(bolinma(5, 10), bo(5, 10));
 
