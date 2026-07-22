e2fe7f6e79541c2b8617b18676b7b6ee396707a9
Azamat-Developer <aroziboyev1066@gmail.com>
2026-07-22 16:40:40 +0500

    add

---
+++ 17-iyul.Functinon/return1.js
@@ -0,0 +1,28 @@
+function userData() {
+  let ism = prompt("Ismingizni kiriting: ")
+  let familiya = prompt("Familiyangizni kiriting: ")
+  let yosh = Number(prompt("Yoshingizni kiriting: "))
+
+  let malumoti = `Sizning ismingiz ${ism}, familiyangiz ${familiya} va yoshingiz ${yosh}da!`
+  
+  console.log(malumoti)
+}
+
+userData()
+
+// _______________________________________________________
+
+function daraja(a) {
+  return a ** 2
+}
+
+console.log(daraja(2))
+
+// _______________________________________________________
+function darajaa(b) {
+  console.log(b ** 3)
+  return "Return funksiyaga qaytaradi"
+}
+
+// darajaa(2)
+console.log(darajaa(2))
