eda0b476becef04d2cb0f8bd600f98c1adfd8bc4
Azamat-Developer <aroziboyev1066@gmail.com>
2026-07-22 16:59:11 +0500

    addd

--- 17-iyul.Functinon/return1.js
+++ 17-iyul.Functinon/return1.js
@@ -1,28 +1,28 @@
-// function userData() {
-//   let ism = prompt("Ismingizni kiriting: ")
-//   let familiya = prompt("Familiyangizni kiriting: ")
-//   let yosh = Number(prompt("Yoshingizni kiriting: "))
+function userData() {
+  let ism = prompt("Ismingizni kiriting: ")
+  let familiya = prompt("Familiyangizni kiriting: ")
+  let yosh = Number(prompt("Yoshingizni kiriting: "))
 
-//   let malumoti = `Sizning ismingiz ${ism}, familiyangiz ${familiya} va yoshingiz ${yosh}da!`
+  let malumoti = `Sizning ismingiz ${ism}, familiyangiz ${familiya} va yoshingiz ${yosh}da!`
   
-//   console.log(malumoti)
-// }
+  console.log(malumoti)
+}
 
 // userData()
 
 // _______________________________________________________
 
-// function daraja(a) {
-//   return a ** 2
-// }
+function daraja(a) {
+  return a ** 2
+}
 
 // console.log(daraja(2))
 
 // _______________________________________________________
-// function darajaa(b) {
-//   console.log(b ** 3)
-//   return "Return funksiyaga qaytaradi"
-// }
+function darajaa(b) {
+  console.log(b ** 3)
+  return "Return funksiyaga qaytaradi"
+}
 
 // darajaa(2)      
 // console.log(darajaa(2))
@@ -32,3 +32,6 @@ function qoshish(a, b) {
     return a + b
 }
 console.log(qoshish(5, 20));
+console.log(qoshish(5)); //! NaN - Not a Number
+
+
