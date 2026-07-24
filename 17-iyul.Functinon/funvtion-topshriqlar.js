e7c0cde654d0fb4714feb6b4e44daad9e8304dff
Azamat-Developer <aroziboyev1066@gmail.com>
2026-07-24 16:52:02 +0500

    add

--- 17-iyul.Functinon/funvtion-topshriqlar.js
+++ 17-iyul.Functinon/funvtion-topshriqlar.js
@@ -47,10 +47,14 @@ function masala3(yosh) {
 // 4-masala: [for] 1 dan berilgan songacha bo'lgan barcha sonlar yig'indisini
 // hisoblovchi funksiya yozing.
 function masala4(son) {
-    for (let i = 1; son >= 10; i++) {
-        
-    }
+    for (let i = 1; son > 10; i++) {
+        i += son
+    }     
+    console.log(son);
+    
 }
+masala4(5)
+
 
 // 5-masala: [for] Berilgan sonning faktorialini hisoblovchi funksiya yozing.
 function masala5(son) {}
