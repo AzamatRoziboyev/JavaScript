62115c7b5070f4197a7e2732a42e567533a5ee5f
Azamat-Developer <aroziboyev1066@gmail.com>
2026-07-24 17:10:49 +0500

    add

--- 17-iyul.Functinon/funvtion-topshriqlar.js
+++ 17-iyul.Functinon/funvtion-topshriqlar.js
@@ -73,7 +73,6 @@ function masala5(son) {
 // 6-masala: [for] 1 dan 100 gacha bo'lgan juft sonlarni massivga
 // yig'ib qaytaruvchi funksiya yozing.
 function masala6() {
-    let son = 1
     for (let i = 1; i <= 100; i++) {
         if (i % 2 == 0) {
             console.log(i);
