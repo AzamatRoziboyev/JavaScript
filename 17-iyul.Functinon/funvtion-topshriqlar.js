0e679d3b309d1cdbfd0b94ef38a1446e3ade1f4c
Azamat-Developer <aroziboyev1066@gmail.com>
2026-07-27 16:38:05 +0500

    add

--- 17-iyul.Functinon/funvtion-topshriqlar.js
+++ 17-iyul.Functinon/funvtion-topshriqlar.js
@@ -213,7 +213,9 @@ function masala20(massiv) {
 
 // 21-masala: [object] Ism va yosh parametrlarini qabul qilib,
 // { ism, yosh } ko'rinishida object qaytaruvchi funksiya yozing.
-function masala21(ism, yosh) { }
+function masala21(ism, yosh) { 
+    
+}
 
 // 22-masala: [object] Berilgan object ichida "ism" xususiyati
 // mavjudligini tekshiruvchi funksiya yozing.
