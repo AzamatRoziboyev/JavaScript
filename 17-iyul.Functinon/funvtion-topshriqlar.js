ade97bda519541416fb437f37cc354f29c0d4182
Azamat-Developer <aroziboyev1066@gmail.com>
2026-07-28 23:00:44 +0500

    add

--- 17-iyul.Functinon/funvtion-topshriqlar.js
+++ 17-iyul.Functinon/funvtion-topshriqlar.js
@@ -333,19 +333,21 @@ function masala31(massiv) {
 // 32-masala: [if-else] Berilgan baho (0-100) ga qarab harf baho
 // ("A", "B", "C", "D", "F") qaytaruvchi funksiya yozing.
 function masala32(baho) { 
-    if (baho > 90) {
+    if (baho >= 90 && baho <= 100) {
         return "A"
-    } else if (baho > 70) {
+    } else if (baho >= 80) {
         return "B"
-    } else if (baho > 60) {
+    } else if (baho >= 70) {
         return "C"
-    } else if ( baho >= 40) {
-        return "d"
-    } else {
+    } else if ( baho >= 60) {
+        return "D"
+    } else if (baho >= 0) {
         return "F"
+    } else {
+        "Notogri baxo"
     }
 }
-console.log(masala32(91));
+console.log(masala32());
 
 
 // 33-masala: [for] Berilgan sonning barcha bo'luvchilarini
