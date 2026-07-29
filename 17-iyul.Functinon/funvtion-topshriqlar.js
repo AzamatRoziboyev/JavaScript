59884482bcac5810c989b96590b6ac890274618c
Azamat-Developer <aroziboyev1066@gmail.com>
2026-07-29 17:27:43 +0500

    qa

--- 17-iyul.Functinon/funvtion-topshriqlar.js
+++ 17-iyul.Functinon/funvtion-topshriqlar.js
@@ -84,7 +84,9 @@ function masala6() {
 
 // 7-masala: [while] Berilgan sonning raqamlar yig'indisini
 // while sikli yordamida hisoblang. Masalan: 123 -> 1+2+3 = 6
-function masala7(son) { }
+function masala7(son) { 
+    
+}
 
 // 8-masala: [while] Berilgan sondan boshlab 1 gacha teskari
 // hisoblovchi (countdown) funksiya yozing, natijalarni massivga yozib qaytaring.
