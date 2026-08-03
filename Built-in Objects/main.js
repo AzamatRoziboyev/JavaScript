f3ad81e76c73bf0d23432944a372b09d21f03168
Azamat-Developer <aroziboyev1066@gmail.com>
2026-08-03 17:03:34 +0500

    add

--- Built-in Objects/main.js
+++ Built-in Objects/main.js
@@ -1,3 +1,3 @@
 //* 1) Argumentsiz - joriy sana va vaqtni oladi
 const hozir = new Date();
-console.log(hozir); // Mon Aug 03 2026 16:31:45 GMT+0500 (Uzbekistan Standard Time)
+console.log(hozir); // Mon Aug 03 2026 16:31:45 GMT+0500 (Uzbekistan Standard Time)             
