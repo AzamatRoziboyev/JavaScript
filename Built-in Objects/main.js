49977a4377dcd69f1888cfc28f7452f2c5967e3e
Azamat-Developer <aroziboyev1066@gmail.com>
2026-08-04 22:55:45 +0500

    add

--- Built-in Objects/main.js
+++ Built-in Objects/main.js
@@ -126,12 +126,6 @@ const sana14 = new Date(2026, 7, 4, 10, 44, 0,)
  const sana15 = new Date(2026, 7)
 //  console.log(sana15);
 
-// const add = "shaxruzbek"
-// add = "azamat"
-// console.log(add);
 
-let add = "shaxruzbek"
-add = "azamat"
-console.log(add);
 
  
