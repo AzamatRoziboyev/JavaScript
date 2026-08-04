0be212659ca7ac1847ffa8c0321265ba2ed830ea
Azamat-Developer <aroziboyev1066@gmail.com>
2026-08-04 22:48:28 +0500

    add

--- Built-in Objects/main.js
+++ Built-in Objects/main.js
@@ -125,4 +125,5 @@ const sana14 = new Date(2026, 7, 4, 10, 44, 0,)
  // &5) Faqat yil va oy (qolganlari default: kun=1, soat=0, minut=0...)
  const sana15 = new Date(2026, 7)
 //  console.log(sana15);
+
  
