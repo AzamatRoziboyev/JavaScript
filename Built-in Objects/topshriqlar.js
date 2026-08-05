9ebbc1bac95811e7b0c67b657573ce363b3b6ff8
Azamat-Developer <aroziboyev1066@gmail.com>
2026-08-05 16:48:37 +0500

    add

--- Built-in Objects/topshriqlar.js
+++ Built-in Objects/topshriqlar.js
@@ -19,3 +19,4 @@ const horirgiSana = new Date().toDateString()
 // console.log(horirgiSana);
 
 //* 6-topshiriq 1970-yil 1-yanvardan hozirgi vaqtgacha bo'lgan millisekundlar sonini konsolga chiqaruvchi dastur yozing.
+const
