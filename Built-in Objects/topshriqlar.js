9dc900f73ff09e4cdbabe27d11d3d717f4f6fe41
Azamat-Developer <aroziboyev1066@gmail.com>
2026-08-05 16:39:32 +0500

    add

--- Built-in Objects/topshriqlar.js
+++ Built-in Objects/topshriqlar.js
@@ -1,1 +1,10 @@
 //* 1-topshiriq Hozirgi sana va vaqtni konsolga chiqaruvchi dastur yozing.
+const sana = new Date()
+// console.log(sana);
+
+//* 2-topshiriq Hozirgi yilni konsolga chiqaruvchi dastur yozing.
+const hozirgiYil = new Date().getUTCFullYear()
+// console.log(hozirgiYil);
+
+
+
