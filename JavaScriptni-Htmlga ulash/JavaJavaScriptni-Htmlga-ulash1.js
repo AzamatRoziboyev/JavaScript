521fde000c2a73f1734d4d1a99b4f24fd0ad42ff
Azamat-Developer <aroziboyev1066@gmail.com>
2026-08-07 17:25:14 +0500

    add

---
+++ JavaScriptni-Htmlga ulash/JavaJavaScriptni-Htmlga-ulash1.js
@@ -0,0 +1,10 @@
+ const box = document.getElementById('box')
+ console.log(box);
+ box.style.backgroundColor = "blue"
+ box.style.borderRadius = "50%"
+ box.style
+
+ const lorem = document.getElementById('lorem')
+ console.log(lorem.length);
+
+ 
