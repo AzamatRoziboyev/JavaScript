038c7993a32b457f1f37159925ed98d99e979a87
Azamat-Developer <aroziboyev1066@gmail.com>
2026-08-07 14:23:04 +0500

    add

--- Built-in Objects/topshriqlar.js
+++ Built-in Objects/topshriqlar.js
@@ -100,6 +100,6 @@ const faydolonuvch = {
     yosh: 25
 }
 const json1 = JSON.stringify(faydolonuvch)
-console.log(json1);
+// console.log(json1);
 
-//* 
+//* 26-topshiriq '{"name":"Ali","age":25}' JSON stringini JavaScript ob'ektiga o'giring va konsolga chiqaring.
