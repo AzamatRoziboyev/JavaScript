cba9c6ec89dbcb7f75a1dbc7f235cea13ee1135f
Azamat-Developer <aroziboyev1066@gmail.com>
2026-08-07 18:05:21 +0500

    add

--- JavaScriptni-Htmlga ulash/JavaJavaScriptni-Htmlga-ulash1.js
+++ JavaScriptni-Htmlga ulash/JavaJavaScriptni-Htmlga-ulash1.js
@@ -1,14 +1,72 @@
- const box = document.getElementById('box')
- console.log(box);
- box.style.backgroundColor = "blue"
- box.style.borderRadius = "50%"
- box.style
+const main1 = document.getElementById('main1')
+const div1 = document.getElementById('div1')
+const div2 = document.getElementById('div2')
+const div3 = document.getElementById('div3')
+const div4 = document.getElementById('div4')
+const div5 = document.getElementById('div5')
+const div6 = document.getElementById('div6')
+//  const box = document.getElementById('box')
+//  console.log(box);
+//  box.style.backgroundColor = "blue"
+//  box.style.borderRadius = "50%"
+//  box.style
 
- const lorem = document.getElementById('lorem')
- console.log(lorem.length);
+//  const lorem = document.getElementById('lorem')
+//  console.log(lorem.length);
 
-const main1 =document.getElementById('main1')
-main1.style.width = "700"
-main1.style.height = "700"
-// main1.style.border = "1px"
- 
+main1.style.width = "1000px"
+main1.style.height = "300px"
+main1.style.border = "1px solid red"
+main1.style.margin = "auto"
+main1.style.display = "flex"
+main1.style.gap = "25px"
+console.log(main1);
+
+div1.style.width = "200px"
+div1.style.height = "200px"
+div1.style.backgroundColor = "red"
+div1.style.borderRadius = "10px"
+div1.style.display = "flex"
+div1.style.alignItems = "center"
+div1.style.justifyContent = "center"
+
+div2.style.width = "200px"
+div2.style.height = "200px"
+div2.style.backgroundColor = "green"
+div2.style.borderRadius = "10px"
+div2.style.display = "flex"
+div2.style.alignItems = "center"
+div2.style.justifyContent = "center"
+
+div3.style.width = "200px"
+div3.style.height = "200px"
+div3.style.backgroundColor = "yellow"
+div3.style.borderRadius = "10px"
+div3.style.display = "flex"
+div3.style.alignItems = "center"
+div3.style.justifyContent = "center"
+
+div4.style.width = "200px"
+div4.style.height = "200px"
+div4.style.backgroundColor = "gold"
+div4.style.borderRadius = "10px"
+div4.style.display = "flex"
+div4.style.alignItems = "center"
+div4.style.justifyContent = "center"
+
+div5.style.width = "200px"
+div5.style.height = "200px"
+div5.style.backgroundColor = "red"
+div5.style.borderRadius = "10px"
+div5.style.display = "flex"
+div5.style.alignItems = "center"
+div5.style.justifyContent = "center"
+
+
+div6.style.width = "200px"
+div6.style.height = "200px"
+div6.style.backgroundColor = "blue"
+div6.style.borderRadius = "10px"
+div6.style.display = "flex"
+div6.style.alignItems = "center"
+div6.style.justifyContent = "center"
