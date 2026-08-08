19546338c425a358b37b9d30c2e08d8af29fc175
Azamat-Developer <aroziboyev1066@gmail.com>
2026-08-08 21:48:01 +0500

    add

--- JavaScriptni-Htmlga ulash/Java-Topshiriqlar1.js
+++ JavaScriptni-Htmlga ulash/Java-Topshiriqlar1.js
@@ -9,7 +9,8 @@ main1.style.width = "1000px"
 main1.style.height = "650px"
 main1.style.border = "1px solid red"
 main1.style.margin = "auto"
-main1.style.display = "gird"
+main1.style.display = "grid"
+main1.style.gridTemplateColumns = "repeat(3 , 1fr)"
 console.log(main1);
 //*======== div
 div.style.width = "300px"
