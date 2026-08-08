d6950ce365a9be3c60d84e004895dbcdc6dc9c7b
Azamat-Developer <aroziboyev1066@gmail.com>
2026-08-08 21:20:35 +0500

    add

--- JavaScriptni-Htmlga ulash/Java-Topshiriqlar1.js
+++ JavaScriptni-Htmlga ulash/Java-Topshiriqlar1.js
@@ -1,10 +1,18 @@
 const main1 = document.getElementById('main1')
 const div = document.getElementById('div')
 main1.style.width = "1000px"
-main1.style.height = "500px"
+main1.style.height = "650px"
 main1.style.border = "1px solid red"
 main1.style.margin = "auto"
 console.log(main1);
 
 div.style.width = "300px"
 div.style.height = "300px"
+div.style.backgroundColor = "red"
+div.style.borderRadius = "10px"
+div.style.color = "white"
+div.style.display = "flex"
+div.style.alignItems = "center"
+div.style.justifyContent = "center"
+div.style.fontSize = "25px"
+
