59a59111d2fcf0c8c91203554d9cbf1d639ab4d8
Azamat-Developer <aroziboyev1066@gmail.com>
2026-08-09 21:56:22 +0500

    add

--- JavaScriptni-Htmlga ulash/Java-Topshiriqlar1.js
+++ JavaScriptni-Htmlga ulash/Java-Topshiriqlar1.js
@@ -37,6 +37,10 @@ box1.style.fontSize = "25px"
 box2.style.width = "150px"
 box2.style.height = "50px"
 box2.style.border = "1px solid red"
+box2.style.fontSize = "25px"
+box2.style.display = "flex"
+box2.style.justifyContent = "center"
+box2
 //* ======= main2
 main2.style.width = "1000px"
 main2.style.height = "650px"
