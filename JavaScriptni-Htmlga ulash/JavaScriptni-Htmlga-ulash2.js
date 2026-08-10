07c2768569a91cf5d4dad260a0f6b0bb725b80ce
Azamat-Developer <aroziboyev1066@gmail.com>
2026-08-10 18:00:54 +0500

    add

--- JavaScriptni-Htmlga ulash/JavaScriptni-Htmlga-ulash2.js
+++ JavaScriptni-Htmlga ulash/JavaScriptni-Htmlga-ulash2.js
@@ -1,25 +1,50 @@
-const div = document.createElement("div")
-document.body.append(div)
-div.innerHTML = "<h1>Shaxruzbek</h1><p>Salom</p>"
-const h1 = document.querySelector("div h1")
-const p = document.querySelector("div p")
-h1.style.color = "green"
-h1.style.fontSize = "130px"
-p.style.color = "blue"
-p.style.fontSize = "100px"
+// const div = document.createElement("div")
+// document.body.append(div)
+// div.innerHTML = "<h1>Shaxruzbek</h1><p>Salom</p>"
+// const h1 = document.querySelector("div h1")
+// const p = document.querySelector("div p")
+// h1.style.color = "green"
+// h1.style.fontSize = "130px"
+// p.style.color = "blue"
+// p.style.fontSize = "100px"
 
-div.style.width = "900px"
-div.style.height = "900px"
-div.style.margin = "50px auto"
-div.style.backgroundColor = "red"
-div.style.color = "white"
-div.style.display = "flex"
-div.style.alignItems = "center"
-div.style.flexDirection = "column"
-div.style.justifyContent = "center"
-div.style.borderRadius = "10px"
+// div.style.width = "900px"
+// div.style.height = "900px"
+// div.style.margin = "50px auto"
+// div.style.backgroundColor = "red"
+// div.style.color = "white"
+// div.style.display = "flex"
+// div.style.alignItems = "center"
+// div.style.flexDirection = "column"
+// div.style.justifyContent = "center"
+// div.style.borderRadius = "10px"
 
 //*======= main1
-const div = document.createElement('div')
-div.innerHTML = "<div><p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora dicta, accusantium asperiores saepe aliquid quas, iusto culpa dolor non beatae ut cupiditate facilis eaque mollitia delectus nemo. Ipsam ut reiciendis rem beatae, qui optio ab exercitationem dicta iure nobis ullam ratione dolorum nostrum dolore recusandae? Facere voluptatibus in non quo?</p></div>"
-p.id = "p"
+const div = document.createElement('div') 
+document.body.append(div)
+
+div.innerHTML= `
+    <div> Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora dicta, accusantium asperiores saepe aliquid quas, iusto culpa dolor non beatae ut cupiditate facilis eaque mollitia delectus nemo. Ipsam ut reiciendis rem beatae, qui optio ab exercitationem dicta iure nobis ullam ratione dolorum nostrum dolore recusandae? Facere voluptatibus in non quo?</div>
+    <div> Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora dicta, accusantium asperiores saepe aliquid quas, iusto culpa dolor non beatae ut cupiditate facilis eaque mollitia delectus nemo. Ipsam ut reiciendis rem beatae, qui optio ab exercitationem dicta iure nobis ullam ratione dolorum nostrum dolore recusandae? Facere voluptatibus in non quo?</div>
+    <div> Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora dicta, accusantium asperiores saepe aliquid quas, iusto culpa dolor non beatae ut cupiditate facilis eaque mollitia delectus nemo. Ipsam ut reiciendis rem beatae, qui optio ab exercitationem dicta iure nobis ullam ratione dolorum nostrum dolore recusandae? Facere voluptatibus in non quo?</div>
+    <div> Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora dicta, accusantium asperiores saepe aliquid quas, iusto culpa dolor non beatae ut cupiditate facilis eaque mollitia delectus nemo. Ipsam ut reiciendis rem beatae, qui optio ab exercitationem dicta iure nobis ullam ratione dolorum nostrum dolore recusandae? Facere voluptatibus in non quo?</div>
+   
+
+`
+div.id = "div"
+const tashqidiv = document.getElementById('div')
+tashqidiv.style.width = "2000px"
+tashqidiv.style.height = "500px"
+tashqidiv.style.border = "1px solid red"
+tashqidiv.style.margin = "100px auto"
+tashqidiv.style.display = "flex"
+
+
+const ichkiDivlar = document.querySelectorAll("ichki")
+
+ichkiDivlar.forEach((ichki) => {
+    ichki.style.border = "2px solid red";
+    ichki.style.width = "1000px"
+    ichki.style.height = "500px"
+    ichki.style.overflow = "scroll"
+});
