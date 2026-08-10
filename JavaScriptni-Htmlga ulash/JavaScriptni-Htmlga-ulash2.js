fdff9054d8203a86836ab49c43f343bd2e0dfd2e
Azamat-Developer <aroziboyev1066@gmail.com>
2026-08-10 17:23:38 +0500

    add

--- JavaScriptni-Htmlga ulash/JavaScriptni-Htmlga-ulash2.js
+++ JavaScriptni-Htmlga ulash/JavaScriptni-Htmlga-ulash2.js
@@ -1,14 +1,22 @@
 const div = document.createElement("div")
-const h1 = document.getElementById('h1')
-div.innerHTML = "<div><h1>Shaxruzbek</h1><p>Salom</p></div>"
 document.body.append(div)
-div.style.width = "300px"
-div.style.height = "300px"
-div.style.margin = "auto"
+div.innerHTML = "<h1>Shaxruzbek</h1><p>Salom</p>"
+const h1 = document.querySelector("div h1")
+const p = document.querySelector("div p")
+h1.style.color = "green"
+h1.style.fontSize = "130px"
+p.style.color = "blue"
+p.style.fontSize = "100px"
+
+div.style.width = "900px"
+div.style.height = "900px"
+div.style.margin = "50px auto"
 div.style.backgroundColor = "red"
 div.style.color = "white"
-div.style.textAlign = "center"
-div.style.borderRadius = "10px"
-h1.id = "h1"
-h1.style.color = "red"
+div.style.display = "flex"
+div.style.alignItems = "center"
+div.style.flexDirection = "column"
+div.style.justifyContent = "center"
 
+
+div.style.borderRadius = "10px"
