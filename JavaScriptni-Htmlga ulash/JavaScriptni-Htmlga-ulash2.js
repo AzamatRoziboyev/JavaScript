04082efcbdd447aac3e8930f5380fa41af8ed1b1
Azamat-Developer <aroziboyev1066@gmail.com>
2026-08-10 17:09:55 +0500

    add

---
+++ JavaScriptni-Htmlga ulash/JavaScriptni-Htmlga-ulash2.js
@@ -0,0 +1,14 @@
+const div = document.createElement("div")
+const h1 = document.querySelector("h1")
+div.innerHTML = "<div><h1>Shaxruzbek</h1><p>Salom</p></div>"
+document.body.append(div)
+div.style.width = "300px"
+div.style.height = "300px"
+div.style.margin = "auto"
+div.style.backgroundColor = "red"
+div.style.color = "white"
+div.style.textAlign = "center"
+div.style.borderRadius = "10px"
+h1.id = "h1"
+h1.style.fontSize = "45px"
+
