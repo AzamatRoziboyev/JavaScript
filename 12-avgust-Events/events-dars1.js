e54f76f8d9f3f5a3288dbe7492e8b5db8bfb5adc
Azamat-Developer <aroziboyev1066@gmail.com>
2026-08-12 17:40:31 +0500

    add

--- 12-avgust-Events/events-dars1.js
+++ 12-avgust-Events/events-dars1.js
@@ -1,21 +1,22 @@
-const div1 = document.getElementById('div1')
-div1.addEventListener("click", () => {
-    console.log("Sichqonchani chap tarafni bir marta bosdingiz !");
-    div1.style.backgroundColor = "red"
-    div1.style.width = "600px"
-    div1.style.height = "200px"
-    div1.style.transition = "1s"
-    div1.innerHTML =`
-    <button style = "width: 100px; height: 60px; border: none; background-color: green; color: white;">Send</button>
+// const div1 = document.getElementById('div1')
+// div1.addEventListener("click", () => {
+//     console.log("Sichqonchani chap tarafni bir marta bosdingiz !");
+//     div1.style.backgroundColor = "red"
+//     div1.style.width = "600px"
+//     div1.style.height = "200px"
+//     div1.style.transition = "1s"
+//     div1.innerHTML =`
+//     <button style = "width: 100px; height: 60px; border: none; background-color: green; color: white;">Send</button>
 
-    `
-})
-const div2 = document.getElementById('div2')
-div2.addEventListener("dblclick", () => {
-     console.log("Sichqonchani chap tarafni ikki marta bosdingiz !");
-})
+//     `
+// })
+// const div2 = document.getElementById('div2')
+// div2.addEventListener("dblclick", () => {
+//      console.log("Sichqonchani chap tarafni ikki marta bosdingiz !");
+// })
 
 const button1 = document.getElementById('button1')
+const arr = document.getElementById('add')
 button1.addEventListener("click", () => {
     console.log("ishladi");
     
@@ -27,6 +28,9 @@ button1.addEventListener("click", () => {
     border-radius: 10px;
     font-size: 25px;
     transition: 1s;
-    >✅ Tolov Qilindi!</button>
+    border: none;
+    ">✅ Tolov Qilindi!</button>
+    
     `
+    arr.play()
 })
