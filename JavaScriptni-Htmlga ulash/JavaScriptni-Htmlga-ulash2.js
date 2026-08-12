d7c34934c5aff3985254e69b2bd51c0cefc63c88
Azamat-Developer <aroziboyev1066@gmail.com>
2026-08-12 16:17:01 +0500

    add

--- JavaScriptni-Htmlga ulash/JavaScriptni-Htmlga-ulash2.js
+++ JavaScriptni-Htmlga ulash/JavaScriptni-Htmlga-ulash2.js
@@ -1,23 +1,23 @@
-const div = document.createElement("div")
-document.body.append(div)
-div.innerHTML = "<h1>Shaxruzbek</h1><p>Salom</p>"
-const h1 = document.querySelector("div h1")
-const p = document.querySelector("div p")
-h1.style.color = "green"
-h1.style.fontSize = "130px"
-p.style.color = "blue"
-p.style.fontSize = "100px"
+// const div = document.createElement("div")
+// document.body.append(div)
+// div.innerHTML = "<h1>Shaxruzbek</h1><p>Salom</p>"
+// const h1 = document.querySelector("div h1")
+// const p = document.querySelector("div p")
+// h1.style.color = "green"
+// h1.style.fontSize = "130px"
+// p.style.color = "blue"
+// p.style.fontSize = "100px"
 
-div.style.width = "900px"
-div.style.height = "900px"
-div.style.margin = "50px auto"
-div.style.backgroundColor = "red"
-div.style.color = "white"
-div.style.display = "flex"
-div.style.alignItems = "center"
-div.style.flexDirection = "column"
-div.style.justifyContent = "center"
-div.style.borderRadius = "10px"
+// div.style.width = "900px"
+// div.style.height = "900px"
+// div.style.margin = "50px auto"
+// div.style.backgroundColor = "red"
+// div.style.color = "white"
+// div.style.display = "flex"
+// div.style.alignItems = "center"
+// div.style.flexDirection = "column"
+// div.style.justifyContent = "center"
+// div.style.borderRadius = "10px"
 
 //*======= main1
 const div = document.createElement('div')
