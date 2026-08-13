65374eb8ac344261aa26792669ff3d9c6c498ef5
Azamat-Developer <aroziboyev1066@gmail.com>
2026-08-13 22:15:50 +0500

    add

--- 12-avgust-Events-kunupga/topshriq-events1.js
+++ 12-avgust-Events-kunupga/topshriq-events1.js
@@ -1,1 +1,15 @@
 const button = document.getElementById('button')
+button.addEventListener("click", () => {
+    console.log("ishladi");
+    button.innerHTML = `
+        <button style = "
+        width: 300px;       
+        height: 60px;
+        background-color:  rgb(15, 131, 15);
+        font-size: 25px;
+        border: none;
+        border-radius: 12px;
+        color: white
+        "> 8/13/2026 </button>
+    `
+})
