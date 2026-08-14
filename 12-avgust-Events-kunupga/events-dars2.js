4b3310a5a0cfcd427c972bca3edb27182121de4d
Azamat-Developer <aroziboyev1066@gmail.com>
2026-08-14 18:04:28 +0500

    add

--- 12-avgust-Events-kunupga/events-dars2.js
+++ 12-avgust-Events-kunupga/events-dars2.js
@@ -1,6 +1,43 @@
-const img1 = document.getElementById('img1')
+const img1 = document.getElementById("img1")
 const img2 = document.getElementById('img2')
 const img3 = document.getElementById('img3')
 const img4 = document.getElementById('img4')
 const img5 = document.getElementById('img5')
 const img6 = document.getElementById('img6')
+const add1 = document.getElementById('add1')
+const add2 = document.getElementById('add2')
+const add3 = document.getElementById('add3')
+const add4 = document.getElementById('add4')
+const add5 = document.getElementById('add5')
+const add6 = document.getElementById('add6')
+
+img1.addEventListener("click", () => {
+    add1.play()
+    console.log("ishladi");
+    
+})
+img2.addEventListener("click", () => {
+    add2.play()
+    console.log("ishladi");
+    
+})
+img3.addEventListener("click", () => {
+    add3.play()
+    console.log("ishladi");
+    
+})
+img4.addEventListener("click", () => {
+    add4.play()
+    console.log("ishladi");
+    
+})
+img5.addEventListener("click", () => {
+    add5.play()
+    console.log("ishladi");
+    
+})
+img6.addEventListener("click", () => {
+    add6.play()
+    console.log("ishladi");
+    
+})
