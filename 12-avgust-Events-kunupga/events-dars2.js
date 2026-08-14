2cea157c94f40040257525bca517ef046a5a3bf7
Azamat-Developer <aroziboyev1066@gmail.com>
2026-08-14 17:40:15 +0500

    add

---
+++ 12-avgust-Events-kunupga/events-dars2.js
@@ -0,0 +1,6 @@
+const img1 = document.getElementById('img1')
+const img2 = document.getElementById('img2')
+const img3 = document.getElementById('img3')
+const img4 = document.getElementById('img4')
+const img5 = document.getElementById('img5')
+const img6 = document.getElementById('img6')
