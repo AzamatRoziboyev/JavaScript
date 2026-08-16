dd7457c1d6868249b1ecad6a65a67773a19271eb
Azamat-Developer <aroziboyev1066@gmail.com>
2026-08-16 17:58:40 +0500

    add

--- 16-avgust-Events-kunupga2/topshriq-events2.js
+++ 16-avgust-Events-kunupga2/topshriq-events2.js
@@ -1,7 +1,20 @@
 const btn1 = document.getElementById('btn1')
 const btn2 = document.getElementById('btn2')
+const btn3 = document.getElementById('btn3')
+const btn4 = document.getElementById('btn4')
+const btn5 = document.getElementById('btn5')
+const btn6 = document.getElementById('btn6')
+const btn7 = document.getElementById('btn7')
+const btn8 = document.getElementById('btn8')
+const btn9 = document.getElementById('btn9')
 const add1 = document.getElementById('add1')
 const add2 = document.getElementById('add2')
+const add3 = document.getElementById('add3')
+const add5 = document.getElementById('add5')
+const add6 = document.getElementById('add6')
+const add7 = document.getElementById('add7')
+const add8 = document.getElementById('add8')
+const add9 = document.getElementById('add9')
 btn1.addEventListener("click", () => {
     console.log("Ishladi");
     btn1.innerHTML = `
@@ -31,4 +44,109 @@ btn2.addEventListener("click", () => {
         " ><i class="fa-solid fa-pause"></i></button>
     `
     add2.play()
+})
+btn3.addEventListener("click", () => {
+    console.log("Ishladi");
+    btn3.innerHTML = `
+        <button style = "
+         width: 50px;
+    height: 50px;
+    font-size: 25px;
+    border: none;
+    border-radius: 50%;
+    background-color: rgb(43, 41, 41);
+    color: white;
+        " ><i class="fa-solid fa-pause"></i></button>
+    `
+    add3.play()
+})
+btn4.addEventListener("click", () => {
+    console.log("Ishladi");
+    btn4.innerHTML = `
+        <button style = "
+         width: 50px;
+    height: 50px;
+    font-size: 25px;
+    border: none;
+    border-radius: 50%;
+    background-color: rgb(43, 41, 41);
+    color: white;
+        " ><i class="fa-solid fa-pause"></i></button>
+    `
+    add4.play()
+})
+btn5.addEventListener("click", () => {
+    console.log("Ishladi");
+    btn5.innerHTML = `
+        <button style = "
+         width: 50px;
+    height: 50px;
+    font-size: 25px;
+    border: none;
+    border-radius: 50%;
+    background-color: rgb(43, 41, 41);
+    color: white;
+        " ><i class="fa-solid fa-pause"></i></button>
+    `
+    add5.play()
+})
+btn6.addEventListener("click", () => {
+    console.log("Ishladi");
+    btn6.innerHTML = `
+        <button style = "
+         width: 50px;
+    height: 50px;
+    font-size: 25px;
+    border: none;
+    border-radius: 50%;
+    background-color: rgb(43, 41, 41);
+    color: white;
+        " ><i class="fa-solid fa-pause"></i></button>
+    `
+    add6.play()
+})
+btn7.addEventListener("click", () => {
+    console.log("Ishladi");
+    btn7.innerHTML = `
+        <button style = "
+         width: 50px;
+    height: 50px;
+    font-size: 25px;
+    border: none;
+    border-radius: 50%;
+    background-color: rgb(43, 41, 41);
+    color: white;
+        " ><i class="fa-solid fa-pause"></i></button>
+    `
+    add7.play()
+})
+btn8.addEventListener("click", () => {
+    console.log("Ishladi");
+    btn8.innerHTML = `
+        <button style = "
+         width: 50px;
+    height: 50px;
+    font-size: 25px;
+    border: none;
+    border-radius: 50%;
+    background-color: rgb(43, 41, 41);
+    color: white;
+        " ><i class="fa-solid fa-pause"></i></button>
+    `
+    add8.play()
+})
+btn9.addEventListener("click", () => {
+    console.log("Ishladi");
+    btn9.innerHTML = `
+        <button style = "
+         width: 50px;
+    height: 50px;
+    font-size: 25px;
+    border: none;
+    border-radius: 50%;
+    background-color: rgb(43, 41, 41);
+    color: white;
+        " ><i class="fa-solid fa-pause"></i></button>
+    `
+    add9.play()
 })
