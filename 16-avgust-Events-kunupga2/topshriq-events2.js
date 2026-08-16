ea3a69ec9717113a28859cdd8858931633aa559a
Azamat-Developer <aroziboyev1066@gmail.com>
2026-08-16 17:08:36 +0500

    add

--- 16-avgust-Events-kunupga2/topshriq-events2.js
+++ 16-avgust-Events-kunupga2/topshriq-events2.js
@@ -1,5 +1,7 @@
 const btn1 = document.getElementById('btn1')
+const btn2 = document.getElementById('btn2')
 const add1 = document.getElementById('add1')
+const add2 = document.getElementById('add2')
 btn1.addEventListener("click", () => {
     console.log("Ishladi");
     btn1.innerHTML = `
@@ -14,4 +16,19 @@ btn1.addEventListener("click", () => {
         " ><i class="fa-solid fa-pause"></i></button>
     `
     add1.play()
+})
+btn2.addEventListener("click", () => {
+    console.log("Ishladi");
+    btn2.innerHTML = `
+        <button style = "
+         width: 50px;
+    height: 50px;
+    font-size: 25px;
+    border: none;
+    border-radius: 50%;
+    background-color: rgb(43, 41, 41);
+    color: white;
+        " ><i class="fa-solid fa-pause"></i></button>
+    `
+    add2.play()
 })
