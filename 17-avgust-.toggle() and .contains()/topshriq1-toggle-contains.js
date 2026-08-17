1fd942e22959c9ab7aa6bf59c9579a13fc4cf709
Azamat-Developer <aroziboyev1066@gmail.com>
2026-08-17 20:13:45 +0500

    add

--- 17-avgust-.toggle() and .contains()/topshriq1-toggle-contains.js
+++ 17-avgust-.toggle() and .contains()/topshriq1-toggle-contains.js
@@ -3,10 +3,10 @@ btn.addEventListener("click", () => {
     document.body.classList.toggle("light")
 
     if (document.body.classList.contains("light")) {
-        btn.style.fontSize = "46px"
-        btn.textContent = `☀️`
+        btn.style.fontSize = "26px"
+        btn.textContent = `☀️ Dark Mode`
     } else {
-        btn.style.fontSize = "46px"
-        btn.textContent =  ``
+        btn.style.fontSize = "26px"
+        btn.textContent =  `🌙 Dark Mode`
     }
 })
