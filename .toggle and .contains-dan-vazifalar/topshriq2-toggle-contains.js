d3336eb69c3c40d3aaa2f7dd0cb1f3916d616f4f
Azamat-Developer <aroziboyev1066@gmail.com>
2026-08-18 22:58:19 +0500

    add

--- .toggle and .contains-dan-vazifalar/topshriq2-toggle-contains.js
+++ .toggle and .contains-dan-vazifalar/topshriq2-toggle-contains.js
@@ -1,7 +1,7 @@
 const btn = document.getElementById('btn')
 btn.addEventListener("click", () => {
     document.body.classList.toggle("dark")
-    
+
     if (document.body.classList.contains("dark")) {
         btn.textContent = `🌑`
     } else {
