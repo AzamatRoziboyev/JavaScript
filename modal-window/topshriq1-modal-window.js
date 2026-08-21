fd1a1d5f2f8e965ab9124808efd35aac0a7a9238
Azamat-Developer <aroziboyev1066@gmail.com>
2026-08-21 18:03:34 +0500

    add

---
+++ modal-window/topshriq1-modal-window.js
@@ -0,0 +1,10 @@
+const login = document.querySelector('.login') 
+const loginBtn = document.querySelector('.login-btn')
+const p1 = document.querySelector('p1')
+loginBtn.addEventListener("click", () => {
+    login.classList.add("active")
+})
+loginBtn.addEventListener("click", () => {
+    p1.classList.remove("active")
+})
+
