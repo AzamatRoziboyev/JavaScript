776e4e6fdab7c1732dada11ba7dc164e4a2650ac
Azamat-Developer <aroziboyev1066@gmail.com>
2026-08-24 17:02:59 +0500

    add

--- Web-storage/dars1-Web-storage.js
+++ Web-storage/dars1-Web-storage.js
@@ -1,2 +1,13 @@
 localStorage.setItem("ism", "Shaxruzbek")
 localStorage.setItem("ism1", "Saidjon")
+
+const ism = localStorage.getItem("ism")
+console.log(ism);
+
+// const ism1 = localStorage.removeItem("ism1")
+// localStorage.clear()
+
+
+
+console.log(localStorage.length);
+
