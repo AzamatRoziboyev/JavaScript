851f2608ac8b95d77db782970072c874e5f8d95f
Azamat-Developer <aroziboyev1066@gmail.com>
2026-08-24 18:10:44 +0500

    add

--- modal-window/modal-window-dars2.js
+++ modal-window/modal-window-dars2.js
@@ -1,3 +1,10 @@
-const bmwBtn = document.querySelector('bmw-btn')
-const bmwKatta = document.querySelector('bmw-katta')
-const closeBtn = document.querySelector
+const bmwBtn = document.querySelector('.bmw-btn')
+const bmwKatta = document.querySelector('.bmw-katta')
+const closeBtn = document.querySelector('.close-btn')
+
+bmwBtn.addEventListener("click", () => {
+    bmwKatta.classList.add("active")
+})
+closeBtn.addEventListener("click", () => {
+    bmwKatta.classList.remove("active")
+})
