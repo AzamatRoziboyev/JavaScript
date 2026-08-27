735750211710c1d9dcd86cc95e560e4f831a346f
Azamat-Developer <aroziboyev1066@gmail.com>
2026-08-27 18:15:14 +0500

    addd

--- Bootstrap/dars1.Bootstrap-5.js
+++ Bootstrap/dars1.Bootstrap-5.js
@@ -9,6 +9,10 @@ const a1Close = document.querySelector('.a1-close')
 const a2 = document.querySelector(".a2")
 const a2Katta = document.querySelector('.a2-katta')
 const a2Close = document.querySelector('.a2-close')
+// * 
+const a3 = document.querySelector(".a3")
+const a3Katta = document.querySelector('.a3-katta')
+const a3Close = document.querySelector('.a3-close')
 
 loginBtn.addEventListener("click", () => {
     loginKatta.classList.add("active")
@@ -29,4 +33,11 @@ a2.addEventListener("click", () => {
 })
 a2Close.addEventListener("click", () => {
     a2Katta.classList.remove("active")
+})
+// *3
+a3.addEventListener("click", () => {
+    a3Katta.classList.add("active")
+})
+a3Close.addEventListener("click", () => {
+    a3Katta.classList.remove("active")
 })
