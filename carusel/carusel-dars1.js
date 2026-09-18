073eb8e763a3d3dd57ea5ae4f0fcfc1355c9a4ef
Azamat-Developer <aroziboyev1066@gmail.com>
2026-09-18 17:26:23 +0500

    add

---
+++ carusel/carusel-dars1.js
@@ -0,0 +1,8 @@
+const track = document.getElementById("track")
+const prevBtn = document.getElementById("prevBtn")
+const nextBtn = document.getElementById("nextBtn")
+
+let hisoblaguich = 0;
+
+const = cardWidth = 210;
+
