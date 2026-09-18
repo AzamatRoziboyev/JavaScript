29bb5879018e8deda94f402ff34b9de232a59a47
Azamat-Developer <aroziboyev1066@gmail.com>
2026-09-18 17:32:19 +0500

    add

--- carusel/carusel-dars1.js
+++ carusel/carusel-dars1.js
@@ -2,7 +2,20 @@ const track = document.getElementById("track")
 const prevBtn = document.getElementById("prevBtn")
 const nextBtn = document.getElementById("nextBtn")
 
-let hisoblaguich = 0;
+let hisoblagich = 0;
 
-const = cardWidth = 210;
+const cardWidth = 210;
+
+nextBtn.addEventListener("click", () => {
+    if(hisoblagich < 3) {
+        hisoblagich++;
+        updateCarousel();
+    }
+})
+prevBtn.addEventListener("click", () => {
+    if(hisoblagich > 0) {
+        hisoblagich--;
+        updateCarousel();
+    }
+})
 
