4311105a03f961acd5e14983f010537e915004ab
Azamat-Developer <aroziboyev1066@gmail.com>
2026-09-18 17:39:42 +0500

    add

--- carusel/carusel-dars1.js
+++ carusel/carusel-dars1.js
@@ -1,13 +1,16 @@
 const track = document.getElementById("track")
 const prevBtn = document.getElementById("prevBtn")
 const nextBtn = document.getElementById("nextBtn")
+const card = document.querySelectorAll(".card")
+const cardlength = card.length // kartalar uzunligi
+
 
 let hisoblagich = 0;
 
 const cardWidth = 210;
 
 nextBtn.addEventListener("click", () => {
-    if(hisoblagich < 3) {
+    if(hisoblagich < cardlength) {
         hisoblagich++;
         updateCarousel();
     }
@@ -19,3 +22,9 @@ prevBtn.addEventListener("click", () => {
     }
 })
 
+function updateCarousel() {
+    // Yo'lakni chapga surish (minus qiymat)
+    const moveAmount = hisoblagich * cardWidth
+    // CSS transform  orqali hatrakatlantirish
+    track.style.transform = `translateX(${moveAmount}px)`
+}
